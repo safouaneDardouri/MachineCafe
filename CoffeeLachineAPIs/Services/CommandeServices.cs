44f08fc6600ddc2acc84c77ce1904982676f53ee
Guere <Guere@DESKTOP-5A7A8H0>
2018-03-12 01:42:00 +0100

    v1

---
+++ CoffeeLachineAPIs/Services/CommandeServices.cs
@@ -0,0 +1,41 @@
+﻿using CoffeeMachine.core.SOLUTIONS.Coffee.Commande;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeLachineAPIs.Services
+{
+    class CommandeServices : ICommandeServices
+    { private CommandeDAO cmd = new CommandeDAO();
+        public List<Commande> AfficherCommandes()
+        {
+            var result = cmd.GetListCommande();
+            return result;
+        }
+
+        public void AjouterCommande(Commande insertObj)
+        {
+             cmd.Add(insertObj);
+        }
+
+        public void MajCommande(Commande updateObj)
+        {
+            cmd.Update(updateObj);
+        }
+
+        public Commande TrouverCommandeparID(int id)
+        {
+            var result = cmd.Find(id);
+            return result;
+
+        }
+
+        public List<Commande> TrouverCommandeparUser(string id)
+        {
+            var result = cmd.getByUser(id);
+            return result;
+        }
+    }
+}
